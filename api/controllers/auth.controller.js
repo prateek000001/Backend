8cbc60486cf8a8ec9ce5044cc7ced1ed5488736c
Prateek Lahari <prateeklahari10@gmail.com>
2025-10-13 02:56:42 +0530

    Save local changes before pull

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -53,6 +53,7 @@ export const signin = async (req, res, next) => {
         sameSite: "none",
         path:'/',
         maxAge: 24 * 60 * 60 * 1000,
+        domain: "backend-grre.onrender.com",
       })
       .status(200)
       .json(rest);
@@ -94,6 +95,7 @@ export const google = async (req, res, next) => {
         secure: true,
         sameSite: "none",
         maxAge: 24 * 60 * 60 * 1000,
+        domain: "backend-grre.onrender.com",
       })
       .status(200)
       .json(rest);
